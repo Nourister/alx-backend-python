1e6fd9fa583838b3a87c370a43f80ece9cb968a1
nourister <nouristerjuma@gmail.com>
2024-04-18 09:41:28 +0000

    0x00-python_variable_annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+""" annotations concat that takes a string str1 and a string str2 """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Returns sum of two strings """
+    return str1 + str2
